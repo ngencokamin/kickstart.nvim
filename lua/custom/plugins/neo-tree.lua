475aa0b4acdf99d7a02f07bf11ea12a82fde7f87
Nix Genco-Kamin <ngencokamin@gmail.com>
2026-03-15 15:54:34 -0500

    neo-tree, grug-far

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -2,11 +2,7 @@ return {
   {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      'nvim-tree/nvim-web-devicons',
-    },
+    dependencies = { 'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim', 'nvim-tree/nvim-web-devicons', 's1n7ax/nvim-window-picker' },
     opts = {
       default_component_configs = {
         git_status = {
@@ -21,17 +17,54 @@ return {
             staged = '',
             conflict = '',
           },
+          align = 'right',
         },
-        align = 'right',
+      },
+      window = {
+        mappings = {
+          ['P'] = {
+            'toggle_preview',
+            config = {
+              use_float = false,
+            },
+          },
+        },
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>n',
+        function()
+          require('neo-tree.command').execute {
+            toggle = true,
+            source = 'filesystem',
+            position = 'float',
+          }
+        end,
+        desc = '[N]eotree floating',
+      },
+      {
+        '<leader>N',
+        function()
+          require('neo-tree.command').execute {
+            toggle = true,
+            source = 'filesystem',
+            position = 'left',
+          }
+        end,
+        desc = '[N]eotree Sidebar',
       },
     },
   },
   {
     'antosha417/nvim-lsp-file-operations',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
-    },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-neo-tree/neo-tree.nvim' },
+    lazy = false,
     config = function() require('lsp-file-operations').setup() end,
   },
   {
@@ -42,11 +75,8 @@ return {
         filter_rules = {
           include_current_win = false,
           autoselect_one = true,
-          -- filter using buffer options
           bo = {
-            -- if the file type is one of following, the window will be ignored
             filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
-            -- if the buffer type is one of following, the window will be ignored
             buftype = { 'terminal', 'quickfix' },
           },
         },
